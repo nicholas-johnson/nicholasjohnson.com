52eb4de3e7ee61acdeb9f75ed03a44ccbe33dc6b
Nicholas Johnson <me@nicholasjohnson.com>
2022-12-02 09:16:11 +0000

    syntax highlighting

--- packages/nicholasjohnson.com/lib/getPostByPath.ts
+++ packages/nicholasjohnson.com/lib/getPostByPath.ts
@@ -1,11 +1,28 @@
 import matter from 'gray-matter'
 import fs from 'fs'
 import { marked } from 'marked'
+import hljs from 'highlight.js'
 
 export const getPostByPath = async (filePath: string) => {
   const fileContents = fs.readFileSync(filePath, 'utf8')
   const { data, content } = matter(fileContents)
   const { date, title } = data
+
+  marked.setOptions({
+    renderer: new marked.Renderer(),
+    highlight: function (code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    },
+    langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+    pedantic: false,
+    gfm: true,
+    breaks: false,
+    sanitize: false,
+    smartypants: false,
+    xhtml: false,
+  })
+
   const html = marked.parse(content)
   const segments = filePath.split('/')
   const slug = segments.pop()?.replace('.md', '')
