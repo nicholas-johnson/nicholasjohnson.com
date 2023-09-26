162f9054bfc63cdd3e27aae01d3f35a989d8b63a
Nicholas Johnson <me@nicholasjohnson.com>
2023-09-26 21:52:47 +0100

    rails and angular courses

---
+++ src/data/angularjsCourse.ts
@@ -0,0 +1,216 @@
+import type { ICourse } from "../types/Course";
+
+export const css3Course: ICourse = {
+  title: "The AngularJS Course",
+  description: "",
+  bullets: [],
+  slug: "",
+  cover: "",
+  coverAlt: "",
+  coverCredit: "",
+  coverCreditUrl: "",
+  version: "",
+  createdAt: "",
+  lastUpdatedAt: "",
+  exercises: [
+    {
+      slug: "angular-intro",
+      title: "Introducing Angular",
+      description: "Course Intro",
+    },
+    {
+      slug: "templates",
+      title: "Hello World",
+      description: "Hello World in Angular couldn't be simpler",
+      github: "angular_templates_demo",
+    },
+    {
+      slug: "binding",
+      title: "Binding",
+      description:
+        "Where we discover the spooky magic that is Angular data binding (of course it's not magic really, as we shall very soon see).",
+      github: "angular_binding_demo",
+    },
+    {
+      slug: "more-binding",
+      title: "More on Binding",
+      description:
+        "In which we bind expressions to classes and attributes and add conditional logic to our templates.",
+      github: "angular_further_binding_demo",
+    },
+    {
+      slug: "controllers",
+      title: "Controllers",
+      description: "Controllers glue the scope to the template.",
+      github: "angular_controllers_demo",
+    },
+    {
+      slug: "scope",
+      title: "Scope and Scope Inheritance (View Model)",
+      description: "$scope is an object in which you can store your models",
+      github: "angular_scope_demo",
+    },
+    {
+      slug: "watch",
+      title: "Watch and the digest cycle",
+      description:
+        "Here we delve into the workings of the digest cycle and discover the logic behind Angular data binding.",
+      github: "angular_watch_demo",
+    },
+    {
+      slug: "di",
+      title: "Dependency Injection (DI)",
+      description:
+        "Dependency Injection allows us to require components simply be referring to them. Learn about DI and also split your code into sensible modules.",
+      github: "angular_di_demo",
+    },
+    {
+      slug: "karma",
+      title: "Unit Testing with Karma (Testacular)",
+      description: "All good developers write unit tests. We will too.",
+      github: "angular_karma_demo",
+    },
+    {
+      slug: "homework",
+      title: "Homework",
+      description: "Fun and simple homework. It's like school",
+    },
+    {
+      slug: "repeat-and-filter",
+      title: "Repeat and Filter",
+      description:
+        "The show-off feature of Angular. This stuff makes clients really happy.",
+      github: "angular_ngrepeat_demo",
+    },
+    {
+      slug: "ajax",
+      title: "AJAX",
+      description:
+        "Drop JSON into your $scope and it will automatically bind it to the template.",
+      github: "angular_ajax_demo",
+    },
+    {
+      slug: "ng-include",
+      title: "Templates and the Template Cache",
+      description:
+        "Talking to the template cache directly can make your app feel more responsive. Learn how.",
+      github: "angular_template_cache_demo",
+    },
+    {
+      slug: "animation",
+      title: "The Animation Framework",
+      description: "Angular will inspect your CSS and modify it's behavior.",
+      github: "angular_animation_demo",
+    },
+    {
+      slug: "custom-filters",
+      title: "Writing Custom Filters",
+      description:
+        "A filter is a function for modifying and presenting data. We learn to write and use our own.",
+      github: "angular_custom_filters_demo",
+    },
+    {
+      slug: "services",
+      title: "Writing Services",
+      description:
+        "Services are singleton objects that act as repositories of useful functions. How to make them, and when to use them.",
+      github: "angular_services_demo",
+    },
+    {
+      slug: "promises",
+      title: "Promises",
+      description: "Tidying up asynchronous code using promises.",
+      github: "angular_promises_demo",
+    },
+    {
+      slug: "directives",
+      title: "Writing Directives",
+      description:
+        "Directives tell the Angular compiler how to transform the DOM. They are the guts of Angular. You will learn all about them.",
+      github: "angular_directives_demo",
+    },
+    {
+      slug: "directive-compilation",
+      title: "Directive Compilation",
+      description:
+        "We look at how the Angular compiler traverses the DOM, oreder of compilation, and when to use the compile, link and controller functions.",
+      github: "angular_directive_compilation_demo",
+    },
+    {
+      slug: "parse",
+      title: "Parsing attributes against $scope",
+      description: "How to make out directives receive an expression.",
+      github: "angular_parse_demo",
+    },
+    {
+      slug: "testing-directives",
+      title: "Unit testing directives using Karma",
+      description: "Unit testing is for directives too.",
+      github: "angular_testing_directives_demo",
+    },
+    {
+      slug: "isolate-scope",
+      title: "Isolate Scopes",
+      description:
+        "One of the most misunderstood features of directives. We learn, in detail, how to control the scope hierarchy.",
+      github: "angular_directive_isolate_scopes_demo",
+    },
+    {
+      slug: "homework-2",
+      title: "Homework 2",
+      description: "Again, like school, but better paid.",
+    },
+    {
+      slug: "transclusion",
+      title: "Directive Transclusion",
+      description:
+        "Transclusion allows us to pass template content to a directive as an attribute. This is a lot more useful than it sounds. Here we see why.",
+      github: "angular_directive_transclusion_demo",
+    },
+    {
+      slug: "crud",
+      title: "Create, Read, Update, Delete",
+      description:
+        "We put it together and talk to a real RESTful API to build a little content driven app",
+      github: "angular_crud_demo",
+    },
+    {
+      slug: "webapi",
+      title: "Build a back end with WebAPI",
+      description: "Build a simple API with WebAPI, then integrate from it.",
+      github: "angular_webapi_demo",
+    },
+    {
+      slug: "validation",
+      title: "Form Validation",
+      description: "We validate our inputs the right way",
+      github: "angular_form_validation_demo",
+    },
+    {
+      slug: "protractor",
+      title: "Protractor",
+      description: "End to end integration testing.",
+      github: "angular_protractor_demo",
+    },
+    {
+      slug: "resource",
+      title: "Resource",
+      description: "Resource is an ORM for REST. Powerful and terse AJAX.",
+      github: "angular_resource_demo",
+    },
+    {
+      slug: "routing",
+      title: "Routing",
+      description:
+        "Learn about routing, Pushstate, and how to make an Angular site SEO friendly without hashbangs",
+      github: "angular_routing_demo",
+    },
+    {
+      slug: "ui-router",
+      title: "UI Router",
+      description:
+        "The UI Router is an alternative state based router for Angular. Learn all about it.",
+      github: "angular_ui_router_demo",
+    },
+  ],
+};
---
+++ src/pages/posts/what-is-conda.md
@@ -0,0 +1,188 @@
+---
+title: What is Conda? What Can It Do?
+tags: python, ai
+layout: "../../layouts/BlogPost.astro"
+date: "2023-09-25"
+preview: "Anaconda gives you three things:
+
+
+1. It's a Python dependency management tool, like NPM. 
+
+2. It's a environment namagement tool like NVM or RVM. 
+
+3. It's a curated repository of a few thousand hand-verified libraries that you can rely on.
+
+
+In this short article we look at Conda, how to use it, how it works with Pip, how to work with environments, and how to load, save, and share environments in a requirements.txt.
+"
+---
+
+Anaconda is a Python dependency managemnt tool. It gives you three things:
+
+1. Dependency Management. It lets you install and remove packages.
+2. Environment isolation. It's a environment management tool like NVM or RVM that lets you create isolated environments with their own dependencies, versions, and Python versions.
+3. Repository of curated libraries. Conda has a few thousand hand-verified libraries that you can rely on.
+
+As you can see, it combines several very useful tools into one helpful package.
+
+## Installing Conda
+
+Installation is easy. A simple double click install on On Windows and OSX, or a shell script on Linux. Instructions here:
+
+<a target="\_blankhref="https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html">Installing Anaconda</a>
+
+## Conda Environments
+
+Typing:
+
+```bash
+conda info
+```
+
+at the command line will list some details about your current conda environment. You should notice something like this:
+
+```bash
+active environment : base
+```
+
+This means we are in the base environment, a general environment for all our projects. Ideally we want to create a specific environment for our project. We can do this with conda create:
+
+```bash
+conda create -n my-project
+```
+
+This creates the environemt, but doesn't start using it. To activate the env, type:
+
+```bash
+conda activate my-project
+```
+
+If you have something like Oh My ZSH running you should see the command prompt change to let you know where you are. Nice.
+
+## Installing packages
+
+We can now install a package from the Anaconda repo with Conda install, thus:
+
+```bash
+conda install jellyfish
+```
+
+Having sone this, we can import it from any python script, and expect it to work:
+
+```python
+import jellyfish
+```
+
+Magic.
+
+Note that Conda environments do not stack. You can't install a library in Base and expect it to be installed everywhere. Conda environments are all isolated one from the other.
+
+## Listing packages
+
+To see all the packages you have installed type:
+
+```bash
+conda list
+```
+
+## Listing environments
+
+To see a list all the environments you have created, type:
+
+```bash
+conda env list
+```
+
+## Deleting en environment
+
+To destroy an environment, type:
+
+```bash
+conda deactivate
+```
+
+Note that this is quite a common thing to do. Destroying and recreating an environment is not a big deal in with Anaconda, especially if you have a requirements.txt.
+
+## Usage with Pip
+
+Conda is fully compatible with Pip. Libraries installed with Pip are local to the package you are currently working inside, and, provided you're using Conda's version of Pip, are not global.
+
+Pip using PyPy, which has a LOT more packages, but some of them might be broken or dangerous. Conda has a select few packages, but all of them work and will not infect your computer with herpes.
+
+```bash
+pip install nose
+```
+
+...makes `nose` available in this environment and no other.
+
+<aside class="box">
+
+## Help, I'm getting ModuleNotFoundErrors after a conda install
+
+This likely means you're using a global version of python rather than Conda's local environment specific version. Try:
+
+```bash
+which python
+```
+
+If you see something like this:
+
+```bash
+which python
+/usr/local/bin/python3
+```
+
+you have a global python, and probably need to hit stack overflow for instructions specific to your OS.
+
+If you see something like:
+
+```bash
+which python
+/Users/nicholas/anaconda3/envs/test-env-1/bin/python
+```
+
+You're all good.
+
+Same deal with Pip.
+
+A common mistage (especially with OSX) is to type `python3` or `pip3` to get Python 3 rather than the system default. You don't have to do this with Conda, it has already taken over the `python` command, and typing `python` will get you the version of Python active in the current environment.
+
+</aside>
+
+## Setting an environment specific version of Python
+
+To use a specific version of Python, you have to create the environment with the python flag:
+
+```bash
+conda create --name test-env-2 python=3.8
+```
+
+You can't change the version of Python in a running environment, you have to make a new one, so you may want to `conda deactivate` first. This won't be painful if you have a requirements.txt file.
+
+## Loading packages from a requirements.txt
+
+If you want to share an environment, you can put all your requirements in a file. By convention this is called requirements.txt, but you can call it anything. It's a simple, newline separated list of all your requirements.
+
+To install from a requirements.txt, first create your environment with conda create, then install from the file, like so:
+
+```bash
+conda install --file requirements.txt
+```
+
+## Saving packages to requirements.txt
+
+You can save your current environment to a requirements.txt file, just by piping `conda list -e` into the file:
+
+```bash
+conda list -e > requirements.txt
+```
+
+You can then recreate this environment easily with:
+
+```bash
+conda install --file requirements.txt
+```
+
+## To sum up
+
+So there we have it. A dependency tool, an environment manager, and a list of curated packages, all in one tidy bundle. Conda is pretty great, and you should probably know about it if you're using Python.
