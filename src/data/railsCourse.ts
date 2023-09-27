171cde0edfc83ad5bfc91729935231d1a8d58100
Nicholas Johnson <me@nicholasjohnson.com>
2023-09-27 09:28:17 +0100

    rails course

---
+++ src/data/railsCourse.ts
@@ -0,0 +1,14 @@
+import type { ICourse } from "../types/Course";
+
+export const railsCourse: ICourse = {
+  title: "The Rails Course",
+  slug: "rails-course",
+  cover: "",
+  description: "",
+  bullets: [],
+  coverAlt: "",
+  version: "",
+  createdAt: "",
+  lastUpdatedAt: "",
+  exercises: [],
+};
